0b1585f662de7ebada476de1c2e6dd144e96630b
talforsher <talforsher1@gmail.com>
2019-02-10 13:25:37 +0200

    Add files via upload

---
+++ MyLocations/precache-manifest.312ec64e420c32a8d081a6b3d1d677cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "86989b5d9f69f623f3aa",
+    "url": "/static/css/main.56bfa6b2.chunk.css"
+  },
+  {
+    "revision": "86989b5d9f69f623f3aa",
+    "url": "/static/js/main.86989b5d.chunk.js"
+  },
+  {
+    "revision": "37f9f18223acee8db2f7",
+    "url": "/static/js/1.37f9f182.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9228058ce7f9c0c7b293f93818654d88",
+    "url": "/static/media/logo.9228058c.svg"
+  },
+  {
+    "revision": "1e671d3ca67621c8b0cb7f76a8762e72",
+    "url": "/index.html"
+  }
+];
